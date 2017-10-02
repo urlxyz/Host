7b496424c8ebc005bf8fe1ed074e81a0bfde4b6c
urlxyz <weburlxyz@gmail.com>
2017-10-02 23:04:11 +0700

    Create defer.js

---
+++ defer.js
@@ -0,0 +1,2 @@
+function sleep(ms){var dt=new Date();dt.setTime(dt.getTime() + ms);while(new Date().getTime()<dt.getTime());}sleep(5000);
+document.getElementById("content").style.visibility="visible"; 
