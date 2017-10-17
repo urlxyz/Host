b0d201874c5e66b22387c951e6d2610fdbbb82c9
urlxyz <weburlxyz@gmail.com>
2017-10-17 15:47:30 +0700

    Update defer.js

--- defer.js
+++ defer.js
@@ -1,2 +1,1 @@
-function sleep(ms){var dt=new Date();dt.setTime(dt.getTime() + ms);while(new Date().getTime()<dt.getTime());}sleep(5000);
-document.getElementById("content").style.visibility="visible"; 
+document.getElementsByClassName('tns-outer').style.visibility="visible"; 
